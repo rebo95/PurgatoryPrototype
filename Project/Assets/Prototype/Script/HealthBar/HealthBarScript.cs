e2627f530ad44d15487a2b85b5841e01b15e6fab
Pablo <pablma11@ucm.es>
2019-02-14 12:12:35 +0100

    Health bar sprite added and sme folders creaetd

---
+++ Project/Assets/Prototype/Script/HealthBar/HealthBarScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+    [SerializeField]
+    private float maxHealth;
+
+    private float currentHealth;
+    public float CurrentHealth { get { { return this.currentHealth; } } set { this.currentHealth = value; } }
+
+    private Image healthBarImage;
+
+    private void Start()
+    {
+        healthBarImage = GetComponent<Image>();
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        healthBarImage.fillAmount = currentHealth / maxHealth;
+    }
+}
