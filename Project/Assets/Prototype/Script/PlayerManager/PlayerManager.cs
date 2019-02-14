48ece2893f79ce2ec95445823d6a30fc5dd7bc96
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2019-02-14 13:52:38 +0100

    damage health trigger

--- Project/Assets/Prototype/Script/PlayerManager/PlayerManager.cs
+++ Project/Assets/Prototype/Script/PlayerManager/PlayerManager.cs
@@ -31,6 +31,14 @@ public class PlayerManager : MonoBehaviour
         
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Weapon")
+        {
+            takeDamage();
+        }
+    }
+
     void EndText()
     {
         if (player_Id == 1)
