5210de4e0d5df10cc737aa5504783b418d698e3c
rebo95 <arebored@ucm.es>
2019-02-14 11:38:37 +0100

    somo changes

--- Project/Assets/Prototype/Script/PlayerMovement.cs
+++ Project/Assets/Prototype/Script/PlayerMovement.cs
@@ -19,6 +19,21 @@ public class PlayerMovement : MonoBehaviour
     PlayerAttack[] C2;
 
 
+
+    public int Player_Id;
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
