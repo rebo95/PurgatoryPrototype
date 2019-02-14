6e3e2f19ab36c68ea90e033203e0b1ead5ca57bb
carl69 <carlmorgenstierne@yahoo.com>
2019-02-14 14:38:20 +0100

    player attacks

--- Project/Assets/Prototype/Script/WeaponCode.cs
+++ Project/Assets/Prototype/Script/WeaponCode.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class WeaponCode : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
+    PlayerAttack attackMove;
+    public PlayerManager PM;
+
+
+    public void FireTheAttack(PlayerAttack atk)
     {
-        
+        StartCoroutine("ColliderOn", atk);
+        attackMove = atk;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ColliderOn(PlayerAttack a)
     {
-        
+
+        yield return new WaitForSeconds(a.PointOfNoReturn);
+
+
+        yield return new WaitForSeconds(a.EndOfTheAttack);
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.GetComponent<PlayerManager>().player_Id != PM.player_Id)
+            {
+                other.GetComponent<PlayerManager>().takeDamage();
+            }
+        }
+
     }
 }
