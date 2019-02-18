db208fa2da859195ee1dc8c0fc32847428d5c826
dotnormalized <j.bergbye@gmail.com>
2019-02-18 19:00:27 +0100

    updated splitscreen system. added canvas to players, updated GameCode to populate player canvas with card sprites from gamemanager's scriptable objects

--- Project/Assets/Prototype/Script/GameCode.cs
+++ Project/Assets/Prototype/Script/GameCode.cs
@@ -16,10 +16,22 @@ public class GameCode : MonoBehaviour
     public PlayerAttack[] P2Combo1;
     public PlayerAttack[] P2Combo2;
 
+    
+
 
     void Start()
     {
+        //Populating Player 1 canvas with sprites from their equipped attacks.
+        GameObject.FindGameObjectWithTag("Player1System").gameObject.transform.GetChild(3).transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.gameObject.GetComponent<Image>().sprite = P1Combo1[0].AttackImage;
+        GameObject.FindGameObjectWithTag("Player1System").gameObject.transform.GetChild(3).transform.GetChild(0).transform.GetChild(0).transform.GetChild(1).transform.gameObject.GetComponent<Image>().sprite = P1Combo1[1].AttackImage;
+        GameObject.FindGameObjectWithTag("Player1System").gameObject.transform.GetChild(3).transform.GetChild(0).transform.GetChild(1).transform.GetChild(0).transform.gameObject.GetComponent<Image>().sprite = P1Combo2[0].AttackImage;
+        GameObject.FindGameObjectWithTag("Player1System").gameObject.transform.GetChild(3).transform.GetChild(0).transform.GetChild(1).transform.GetChild(1).transform.gameObject.GetComponent<Image>().sprite = P1Combo2[1].AttackImage;
         
+        //Populating Player 2 canvas with sprites from their equipped attacks.
+        GameObject.FindGameObjectWithTag("Player2System").gameObject.transform.GetChild(3).transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.gameObject.GetComponent<Image>().sprite = P2Combo1[0].AttackImage;
+        GameObject.FindGameObjectWithTag("Player2System").gameObject.transform.GetChild(3).transform.GetChild(0).transform.GetChild(0).transform.GetChild(1).transform.gameObject.GetComponent<Image>().sprite = P2Combo1[1].AttackImage;
+        GameObject.FindGameObjectWithTag("Player2System").gameObject.transform.GetChild(3).transform.GetChild(0).transform.GetChild(1).transform.GetChild(0).transform.gameObject.GetComponent<Image>().sprite = P2Combo2[0].AttackImage;
+        GameObject.FindGameObjectWithTag("Player2System").gameObject.transform.GetChild(3).transform.GetChild(0).transform.GetChild(1).transform.GetChild(1).transform.gameObject.GetComponent<Image>().sprite = P2Combo2[1].AttackImage;
     }
 
     void Update()
