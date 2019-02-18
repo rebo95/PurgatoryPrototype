09850eafc31cb519f7c22d1ecd62b2ad7e44d0e4
dotnormalized <j.bergbye@gmail.com>
2019-02-18 14:07:28 +0100

    Updated player prefab with animation events, sfx for sloppy stab

--- Project/Assets/Prototype/Script/Audio_PlayerFeedback.cs
+++ Project/Assets/Prototype/Script/Audio_PlayerFeedback.cs
@@ -6,6 +6,7 @@ public class Audio_PlayerFeedback : MonoBehaviour
 {
 
     public AudioClip LungeAttack;
+    public AudioClip SloppyStab;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,11 @@ public class Audio_PlayerFeedback : MonoBehaviour
     public void LungeSFX()
     {
         gameObject.transform.GetChild(3).transform.GetChild(0).GetComponent<AudioSource>().PlayOneShot(LungeAttack);
+    }
+    // Sloppy Stab
+    public void SloppySFX()
+    {
+        gameObject.transform.GetChild(3).transform.GetChild(0).GetComponent<AudioSource>().PlayOneShot(SloppyStab);
     }
 
     // Update is called once per frame
