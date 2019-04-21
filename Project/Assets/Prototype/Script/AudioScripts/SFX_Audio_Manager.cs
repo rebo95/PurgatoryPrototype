84b2664fd8116ea3672307eca3c6330419b81fc4
rebo95 <arebored@ucm.es>
2019-04-21 12:55:12 +0200

    bla

---
+++ Project/Assets/Prototype/Script/AudioScripts/SFX_Audio_Manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX_Audio_Manager : MonoBehaviour
+{
+
+    public AudioSource warrior1;
+    public AudioSource warrior2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playWarrior1Audio();
+        Invoke("playWarrior2Audio", warrior1.clip.length + 0.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void playWarrior1Audio()
+    {
+        warrior1.Play();
+    }
+
+    void playWarrior2Audio()
+    {
+        warrior2.Play();
+    }
+}
