49bb5b7620f9ee1051be9479b139e5f5899dfaf5
rebo95 <arebored@ucm.es>
2019-04-21 13:27:37 +0200

    audioAdding

---
+++ Project/Assets/Prototype/Script/AudioScripts/Intro_Scene_SFX_Audio_Manager.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro_Scene_SFX_Audio_Manager : MonoBehaviour
+{
+    public AudioSource aaron1;
+    public AudioSource aaron2;
+    public AudioSource aaron3;
+    public AudioSource aaron4;
+    public AudioSource aaron5;
+    public AudioSource aaron6;
+    public AudioSource aaron7;
+    public AudioSource aaron8;
+    public AudioSource aaron9;
+    public AudioSource aaron10;
+
+
+    public AudioSource pablo1;
+    public AudioSource pablo2;
+    public AudioSource pablo3;
+    public AudioSource pablo4;
+    public AudioSource pablo5;
+    public AudioSource pablo6;
+    public AudioSource pablo7;
+    public AudioSource pablo8;
+    public AudioSource pablo9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void playAaronAudio1()
+    {
+        aaron1.Play();
+    }
+
+    void playAaronAudio2()
+    {
+
+    }
+
+    void playAaronAudio3()
+    {
+
+    }
+
+    void playAaronAudio4()
+    {
+
+    }
+
+    void playAaronAudio5()
+    {
+
+    }
+
+    void playAaronAudio6()
+    {
+
+    }
+
+    void playAaronAudio7()
+    {
+
+    }
+
+    void playAaronAudio8()
+    {
+
+    }
+
+    void playAaronAudio9()
+    {
+
+    }
+
+    void plaPabloAudio1()
+    {
+
+    }
+
+
+    void playPabloAudio2()
+    {
+
+    }
+
+    void playPabloAudio3()
+    {
+
+    }
+
+    void playPabloAudio4()
+    {
+
+    }
+
+    void playPabloAudio5()
+    {
+
+    }
+
+    void playPabloAudio6()
+    {
+
+    }
+
+    void playPabloAudio7()
+    {
+
+    }
+
+    void playPabloAudio8()
+    {
+
+    }
+
+    void playPabloAudio9()
+    {
+
+    }
+}
